3a97b13389d4aefbdd701d3b8300422937557444
zxingwork <zxingwork@163.com>
2020-07-19 22:25:22 +0800

    dependent

--- api2/dependent.sh
+++ api2/dependent.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+echo '[pytest]'
 pip install pytest -U;
+echo '[pymysql]'
 pip install pymysql -U;
+echo '[flask]'
 pip install flask -U;
+echo '[flask-cors]'
 pip install flask-cors -U;
